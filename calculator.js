684d3bfaec187a5cb95f04d5fb1497f9ac4b82af
Rabin-thakur <80481713+Rabin-thakur@users.noreply.github.com>
2024-12-01 23:13:25 +0530

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,3 +1,8 @@
 function getvalue(val){
 document.getElementById("a").value +=val; 
 }
+function add(){
+  let x =  document.getElementById("a").value;
+  var y= eval(x);
+  document.getElementById("a").value=y;
+}
